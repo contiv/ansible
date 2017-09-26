c96afc5a9a1263712451bf355ccd257ea3fe29dc
Jeewan Rana <jerana@cisco.com>
2017-09-26 11:52:45 -0700

    enable contiv-v2plugin on docker service restart
    update docker command with sudo
    
    include review comments

---
+++ roles/contiv_network/files/v2plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+while [ true ]
+do
+        ID="$(docker plugin ls  | awk '/contiv/ {print $2}')"
+        STATUS="$(docker plugin ls  | awk '{print $8}')"
+        if [ $STATUS != true ]; then
+                docker plugin enable $ID
+                sleep 1
+        else
+                break
+        fi
+done
