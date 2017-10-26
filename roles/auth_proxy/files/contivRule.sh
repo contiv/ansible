31edaaf5239b8ec8de51adc9d7b7cee94b739f29
Chris Plock <chrisplo@cisco.com>
2017-10-26 11:16:55 -0700

    contiv iptables rules apply after firewalld
    If firewalld is started, run contivRule.sh after it, but delay it a few
    seconds to let firewalld acquire the iptables lock.  Add -w to wait for
    lock release so it can update.
    
    Signed-off-by: Chris Plock <chrisplo@cisco.com>

--- roles/auth_proxy/files/contivRule.sh
+++ roles/auth_proxy/files/contivRule.sh
@@ -3,13 +3,12 @@ set -e
 IFS="
 "
 rules_file="/etc/contiv/rules.conf"
-if [ -f "$rules_file" ]
-then
+if [ -f "$rules_file" ]; then
     while read line; do
-        eval iptables $line
+        eval iptables -w 10 $line
     done < $rules_file
 else 
     mkdir -p "/etc/contiv"
     touch $rules_file 
-    iptables -S | grep "contiv" > $rules_file
+    iptables -S | sed '/contiv/!d;s/^-A/-I/' > $rules_file
 fi
